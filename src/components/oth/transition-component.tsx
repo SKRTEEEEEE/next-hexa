d670c77d6e5694435cd47addac84fecc9ff3bad2
SKRTEEEEEE <adanreh.m@gmail.com>
2024-11-20 17:08:15 +0100

    test(nextjs15): adapted framer-motions components for nextjs15

--- src/components/oth/transition-component.tsx
+++ src/components/oth/transition-component.tsx
@@ -19,6 +19,7 @@ export function MotionTransition(props: MotionTransitionProps) {
             animate="visible"
             exit="hidden"
             className={className}
+            {...(motion.div as any)}
         >
             {children}
         </motion.div>
